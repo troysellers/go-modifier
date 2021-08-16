124a3ef38b51542ddd9aa61b8d6a76cb5c808000
troysellers <tsellers@grax.com>
2021-08-17 09:46:57 +1000

    start on leads

--- mockaroo/leadSchema.go
+++ mockaroo/leadSchema.go
@@ -1,8 +1,26 @@
 package mockaroo
 
-import "github.com/troysellers/go-modifier/mockaroo/types"
+import (
+	"log"
+
+	"github.com/troysellers/go-modifier/mockaroo/types"
+)
 
 func getSchmeaForLead(fields []interface{}, personAccounts bool) []types.IField {
 	var mockFields []types.IField
+	for _, f := range fields {
+		field := f.(map[string]interface{})
+		if shouldGetData(field) {
+			switch field["name"].(string) {
+			case "Jigsaw", "DandbCompanyId", "IndividualId":
+				log.Printf("Skipping %v - yet to be handled \n", field["name"])
+			default:
+				mf := getMockTypeForField(field)
+				if mf != nil {
+					mockFields = append(mockFields, mf)
+				}
+			}
+		}
+	}
 	return mockFields
 }
